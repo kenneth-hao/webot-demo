176743bfbc21fd7990b174418af5435e1b32b3a1
kenneth-hao <drmrontheway@gmail.com>
2015-07-17 11:43:57 +0800

    add require js-yaml

--- rules/dialogs/index.js
+++ rules/dialogs/index.js
@@ -1,9 +1,11 @@
 module.exports = function(webot) {
 
-webot.dialog(
-  'basic.yaml', 'gags.yaml', 'greetings.js', 'love.yaml',
-  'praise.yaml', 'bad.yaml', 'lonely.yaml', 'sad.yaml',
-  'flirt.yaml', 'emoji.yaml', 'short.yaml'
-);
+  require('js-yaml');
+
+  webot.dialog(
+    'basic.yaml', 'gags.yaml', 'greetings.js', 'love.yaml',
+    'praise.yaml', 'bad.yaml', 'lonely.yaml', 'sad.yaml',
+    'flirt.yaml', 'emoji.yaml', 'short.yaml'
+  );
 
 };
